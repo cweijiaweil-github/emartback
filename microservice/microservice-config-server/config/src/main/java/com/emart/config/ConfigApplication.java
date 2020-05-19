c8b467bc8110222eae493d740081083feb5e0174
cweijiaweil-github <57512674+cweijiaweil-github@users.noreply.github.com>
2020-05-19 17:02:22 +0800

    ''

--- microservice/microservice-config-server/config/src/main/java/com/emart/config/ConfigApplication.java
+++ microservice/microservice-config-server/config/src/main/java/com/emart/config/ConfigApplication.java
@@ -3,11 +3,12 @@ package com.emart.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@SpringBootApplication
 @EnableConfigServer
-// @EnableEurekaClient
+@EnableEurekaClient
+@SpringBootApplication
 public class ConfigApplication {
 
 	public static void main(String[] args) {
